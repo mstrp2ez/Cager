19d6249b38aac52bab5045b3e742d54b12facd1b
mstrp <mstrp2ez@gmail.com>
2016-06-03 17:38:26 +0200

    Removed backend
    Backend should not be under git

--- backend/sqlitequery.php
+++
@@ -1,1 +0,0 @@
-<?phpfunction Query($p_Query){	try	{	$db = new PDO('sqlite:main.db');	$res=$db->query($p_Query);	$db=null;	return $res;	}	catch (PDOException $e)	{		echo 'Exception: ' . $e->getMessage();	}}?>
